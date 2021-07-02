c963e88156639bb5d9c19e28d8995b356a08ec1c
Cyrus Yip <cyruscmyip1@gmail.com>
2021-07-02 12:34:14 -0700

    base toggleposition command

--- cogs/setup.py
+++ cogs/setup.py
@@ -27,6 +27,26 @@ class Setup(commands.Cog):
             f"Created voice channel with an id of `{channel.id}`. You can rename and move it and I'll automatically create voice channels!"
         )
 
+    @commands.command(aliases=["tp"])
+    @commands.has_permissions(manage_guild=True)
+    @commands.guild_only()
+    async def toggleposition(self, ctx: Context):
+        """
+        Change the position of created channels.
+
+        You have to be in a channel created by the auto voice channel you want to change.
+        ~
+        {prefix}toggleposition
+        """
+        # server = await self.bot.get_server(ctx.guild.id)
+        # server["autochannels"][str(channel.id)] = None
+        # await self.bot.servers.find_one_and_update(
+        #     {"_id": str(ctx.guild.id)},
+        #     {"$set": {"autochannels": server["autochannels"]}},
+        #     upsert=True,
+        # )
+        await ctx.send("a")
+
 
 def setup(bot):
     bot.add_cog(Setup(bot))
